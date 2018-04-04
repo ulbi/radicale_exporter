95b6a755be14e9894cbc65c5bdd84d4a05b397c5
danielb <git@masterunit.net>
2018-04-04 19:01:27 +0200

    use handlekeeper for filehandle stability

--- main.go
+++ main.go
@@ -5,23 +5,20 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"os"
-	"path"
 	"regexp"
 	"time"
 
+	"github.com/danielb42/handlekeeper"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/tywkeene/go-fsevents"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	inputFile *os.File
-
 	prm_listenAddr   string
 	prm_inputFile    string
 	prm_scrapeIntvl  int
 	prm_radicaleAddr string
+	prm_debug        bool
 
 	mtr_up = prometheus.NewGauge(
 		prometheus.GaugeOpts{
@@ -47,6 +44,10 @@ var (
 func inspectLine(line string) {
 	pattern := `INFO: (.+) request at (.+/.+)/(.+)? received`
 	result := regexp.MustCompile(pattern).FindStringSubmatch(line)
+
+	if prm_debug {
+	}
+	println("parsed:", line)
 
 	if result != nil {
 		mtr_requests.WithLabelValues(result[1], result[2]).Inc()
@@ -79,59 +80,31 @@ func parseFlags() {
 	kingpin.Flag("inputfile", "exporter input file (truncated!)").Short('i').Default("/var/log/radicale/radicale_exporter_input.log").StringVar(&prm_inputFile)
 	kingpin.Flag("scrapeinterval", "Prometheus scrape interval").Short('s').Default("15").IntVar(&prm_scrapeIntvl)
 	kingpin.Flag("radicale", "address:port to contact Radicale on").Short('r').Default(":5232").StringVar(&prm_radicaleAddr)
+	kingpin.Flag("debug", "print parsed lines").Short('d').Default("false").BoolVar(&prm_debug)
 	kingpin.CommandLine.HelpFlag.Hidden()
 	kingpin.Parse()
-}
-
-func openInputFile() {
-	var err error
-	inputFile, err = os.OpenFile(prm_inputFile, os.O_RDWR|os.O_CREATE, 0644)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func inotifyListener() {
-	options := &fsevents.WatcherOptions{
-		Recursive: false,
-	}
-
-	w, err := fsevents.NewWatcher(path.Dir(prm_inputFile), fsevents.FileRemovedEvent, options)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.StartAll()
-	go w.Watch()
-
-	for {
-		event := <-w.Events
-
-		if event.IsFileRemoved() && event.Path == prm_inputFile {
-			openInputFile()
-		}
-	}
 }
 
 func main() {
 	parseFlags()
 	serveMetrics()
-	go inotifyListener()
 
-	openInputFile()
-	defer inputFile.Close()
+	err := handlekeeper.OpenFile(prm_inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handlekeeper.Close()
 
 	for {
 		if checkTCP() {
-			scanner := bufio.NewScanner(inputFile)
+			scanner := bufio.NewScanner(handlekeeper.InputFile)
 
 			for scanner.Scan() {
 				inspectLine(scanner.Text())
 			}
 
-			inputFile.Truncate(0)
-			inputFile.Seek(0, 0)
+			handlekeeper.InputFile.Truncate(0)
+			handlekeeper.InputFile.Seek(0, 0)
 		}
 
 		time.Sleep(time.Duration(prm_scrapeIntvl) * time.Second)
